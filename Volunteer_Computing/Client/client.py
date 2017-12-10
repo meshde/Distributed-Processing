ef844be78bb2ec0f6834a82346214d45198e42df
meshde <meshde.md@gmail.com>
2017-12-10 18:36:29 +0530

    Enqueue/Dequeue and Publish/Subscribe Model Working

--- Volunteer_Computing/Client/client.py
+++ Volunteer_Computing/Client/client.py
@@ -1,4 +1,5 @@
 import os
+import pika
 
 def get_task():
 	os.system("curl http://localhost:5000/mac > task")
@@ -18,5 +19,38 @@ def main():
 	execute()
 	return
 
+def dequeue():
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+	channel = connection.channel()
+
+	channel.queue_declare(queue='hello')
+
+	def callback(ch, method, properties, body):
+	    print(" [x] Received %r" % body)
+
+	channel.basic_consume(callback,queue='hello',no_ack=True)
+
+	print(' [*] Waiting for messages. To exit press CTRL+C')
+	channel.start_consuming()
+	return
+
+def subscribe():
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+	channel = connection.channel()
+
+	channel.exchange_declare(exchange='tasks',exchange_type='fanout')
+
+	queue = channel.queue_declare(exclusive=True)
+	queue_name = queue.method.queue
+	channel.queue_bind(exchange='tasks',queue = queue_name)
+
+	print(' [*] Waiting for tasks. To exit press CTRL+C')
+
+	def callback(ch, method, properties, body):
+	    print(" [x] %r" % body)
+
+	channel.basic_consume(callback,queue=queue_name,no_ack=True)
+	channel.start_consuming()
+
 if __name__ == '__main__':
-	main()
+	subscribe()
--- Volunteer_Computing/Server/server.py
+++ Volunteer_Computing/Server/server.py
@@ -2,6 +2,7 @@ import os
 import io
 from flask import Flask
 from flask import send_file
+import pika
 
 BASE = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
@@ -20,6 +21,34 @@ def get_task():
 @app.route('/data')
 def get_data():
 	return send_file('hello.txt',attachment_filename='data.txt',mimetype='text/plain')
+
+@app.route('/enqueue')
+def enqueue():
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+	channel = connection.channel()
+
+	channel.queue_declare(queue='hello')
+
+	channel.basic_publish(exchange='',
+	                      routing_key='hello',
+	                      body='Hello World!')
+	print(" [x] Sent 'Hello World!'")
+	connection.close()
+	return "OK\n"
+
+@app.route('/assign')
+def assign_task():
+	connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+	channel = connection.channel()
+
+	channel.exchange_declare(exchange='tasks',exchange_type='fanout')
+	message = "matrix_mac"
+
+	channel.basic_publish(exchange='tasks',routing_key='',body=message)
+	print(" [x] Sent %r" % message)
+
+	connection.close()
+	return "OK\n"
 
 def main():
 	app.run(debug=True)
